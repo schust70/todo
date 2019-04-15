bce8d764ccd4fa7b2d51523ce37a2c8bf6bf08b9
schust70 <schust70@msu.edu>
2019-04-15 15:03:17 -0400

    added ability to add todo list, add todo items to lists, and delete todo items

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
+resources :todo_lists do
+   resources :todo_items
+  end
+root "todo_lists#index"
+end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
@@ -53,4 +57,3 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-end
